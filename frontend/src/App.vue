f2e6c81b0fa2f7e447c42e4feb13076985e5a841
Mica56 <tayotomichael18@gmail.com>
2021-12-22 21:43:13 +0800

    some changes

---
+++ frontend/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <!-- Navigation-->
+    <nav class="navbar navbar-dark" style="background-color: #6f42c1;">
+      <!-- Navbar content -->
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img src="#" alt="" width="30" height="24" class="d-inline-block align-text-top">
+            Medzip
+        </a>
+        <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button> -->
+        <!-- <div class="collapse navbar-collapse" id="navbarNavAltMarkup"> -->
+          <div class="nav justify-content-center">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link" href="#">About Us</a>
+            <a class="nav-link" href="#">Contact Us</a>
+            <a class="nav-link disabled">Disabled</a>
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown link
+            </a>
+            <button class="btn btn-outline-primary me-2" type="button">Login</button>
+          </div>
+        <!-- </div> -->
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+.btn-outline-primary{
+  color: white;
+}
+</style>
