a55122e399de3bcf2fe326ca7ea7a0f4f699b863
Mica56 <tayotomichael18@gmail.com>
2022-03-09 19:32:55 +0800

    Fixed patient and provider pseudoview

--- frontend/src/views/PatientView.vue
+++ frontend/src/views/PatientView.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-      <h1>This is Admin view</h1>
+      <h1>This is Patient view</h1>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'view-auth',
+    name: 'PatientView',
 }
 </script>
 
--- frontend/src/views/ProviderView.vue
+++ frontend/src/views/ProviderView.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-      <h1>This is Guest view</h1>
+      <h1>This is Provider view</h1>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'view-guest',
+    name: 'ProviderView',
 }
 </script>
 
