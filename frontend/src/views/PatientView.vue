1702b826859a0ee86868bf16f4122d17732fbf55
Mica56 <tayotomichael18@gmail.com>
2022-03-18 00:57:03 +0800

    Fixed empty links on Provider/Patient view

--- frontend/src/views/PatientView.vue
+++ frontend/src/views/PatientView.vue
@@ -14,7 +14,7 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav justify-content-center ms-auto py-4 py-lg-0" >
-            <a class="nav-link" aria-current="page"><router-link style="text-decoration: none; color: inherit;" to="/">Home</router-link></a>
+            <a class="nav-link" aria-current="page"><router-link style="text-decoration: none; color: inherit;" to="/home">Home</router-link></a>
             <a class="nav-link" href="#about">About</a>
             <a class="nav-link"><router-link style="text-decoration: none; color: inherit;" to="/partners">Partner Company</router-link></a>
             <a class="nav-link"><router-link style="text-decoration: none; color: inherit;" to="/providers">Providers</router-link></a>
@@ -26,30 +26,18 @@
       </div>
     </nav>
     <router-view/>
-    <!-- Masthead-->
-         <img src="../assets/IMG.png" class="rounded float-end" alt="right" id="right">
-        <header class="masthead">
-            <div class="container">
-                <div class="masthead-heading text-uppercase fs-3">We provide easy solutions to your Healthcare needs</div>
-                <div class="masthead-subheading fs-4">Medzip is a platform for patients, healthcare providers, and health care companies to utilize the safety and security of blockchain database</div><br>
-                <a class="btn btn-primary btn-xl text-uppercase" href="#">GET STARTED</a>
-            </div>
-        </header>
 
-    <AboutUs></AboutUs>
     <Footer></Footer>
   </div>
 </template>
 
 <script>
-import AboutUs from './AboutUs.vue'
 import Footer from './footer.vue'
 
 export default {
   name: 'PatientView',
   components: {
     Footer, 
-    AboutUs,
   },
 }
 </script>
