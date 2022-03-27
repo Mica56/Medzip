3f8592c7d0547079de2077bb52936e14d7cfb1a1
jayyyyemmmm <jayyyyemmmm@gmail.com>
2022-03-27 15:45:46 +0800

    typo fix

--- frontend/src/components/Userspecific/doctorresponse.vue
+++ frontend/src/components/Userspecific/doctorresponse.vue
@@ -4,7 +4,7 @@
 <div class="modal-dialog">
 <div class="modal-content">
 <div class="modal-header">
-<h5 class="modal-title" id="Doctorresponse">Modal title</h5>
+<h5 class="modal-title" id="Doctorresponse">Doctor response</h5>
 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 </div>
 <div class="modal-body">
