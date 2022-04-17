9bfb1408dd438218e4df4fc1813610092875e91d
Mica56 <tayotomichael18@gmail.com>
2022-04-18 01:32:38 +0800

    some changes

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,4 +9,4 @@ createApp(App).use(router).mount('#app')
 // app.config.globalProperties.$isPatient = false
 // app.config.globalProperties.$isProvider = false
 
-export const EventBus = new Vue();
+// export const EventBus = new Vue();
